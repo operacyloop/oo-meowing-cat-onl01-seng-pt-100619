f9d9843f38a92f1f2c7207f9bf99f15c4be5b675
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-11-01 04:22:16 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,7 +1,18 @@
+class Cat 
+  
+  attr_reader :name
+  attr_writer :name
+  
+  def meow
+    puts "meow"
+  end
+ 
+end
+
 maru = Cat.new
 maru.name = "Maru"
  
-maru.name
+p maru.name
 # => "Maru"
  
 maru.meow
