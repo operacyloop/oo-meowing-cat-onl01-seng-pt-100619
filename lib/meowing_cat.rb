e5d4f5f95901147488e483913e31d4d1afa9db50
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-11-01 04:27:09 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,19 @@
-## code your solution here. 
+class Cat 
+  
+  attr_accessor :name
+  
+  def meow
+    puts "meow!"
+  end
+ 
+end
+
+# = Cat.new
+#maru.name = "Maru"
+ 
+#p maru.name
+# => "Maru"
+ 
+#maru.meow
+# "meow!"
+# => nil
